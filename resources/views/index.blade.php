6d9ad7f473bd9689031228e423329b1ba8b4f195
Hector Zurga <hjzurga@gmail.com>
2019-06-23 16:28:06 -0400

    actualizando lorem ipsum

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,11 +13,11 @@
   </p>
 
   <p class="description">
-    In a pellentesque arcu. Mauris sit amet lobortis metus, quis tincidunt risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In ac semper mauris. Morbi iaculis a mauris sit amet vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ligula ipsum, porta consectetur nisl bibendum, sagittis dignissim nunc. Mauris molestie mi aliquet viverra tempor. Mauris nec tristique arcu. Duis vitae nibh pretium nunc pharetra elementum. Nulla ac est metus. Aliquam venenatis porta lorem quis vestibulum. Cras tempor purus et elit lobortis tincidunt. Quisque ullamcorper faucibus velit, eu ultrices risus feugiat vitae. Sed dolor libero, condimentum ut ultricies nec, scelerisque ac mi. Morbi augue tortor, aliquam eu fringilla ac, rhoncus sed turpis. Integer in purus sem. Etiam ac tincidunt nisl.
   </p>
 
   <p class="description">
-    Vestibulum eros leo, semper eget lacus vel, rhoncus eleifend felis. Proin facilisis sem magna, mattis dapibus lectus facilisis in. Etiam suscipit tristique hendrerit. Vivamus finibus, enim nec ultrices venenatis, diam nunc vulputate nisi, nec semper leo nisi ut odio. Phasellus accumsan efficitur est, quis ultricies lorem aliquet at. Nulla non vulputate diam. Curabitur ornare dignissim lacus ut rhoncus. Duis eget volutpat arcu, eget fermentum nibh. Fusce ultricies imperdiet libero, dictum faucibus erat mollis quis. Vivamus tincidunt, nibh sed varius molestie, magna neque blandit neque, ut molestie dolor felis in erat. Nulla vitae bibendum odio. Etiam mollis vitae dolor at imperdiet. Pellentesque rutrum tellus ut diam euismod, quis efficitur nisl sodales.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ligula ipsum, porta consectetur nisl bibendum, sagittis dignissim nunc. Mauris molestie mi aliquet viverra tempor. Mauris nec tristique arcu. Duis vitae nibh pretium nunc pharetra elementum. Nulla ac est metus. Aliquam venenatis porta lorem quis vestibulum. Cras tempor purus et elit lobortis tincidunt. Quisque ullamcorper faucibus velit, eu ultrices risus feugiat vitae. Sed dolor libero, condimentum ut ultricies nec, scelerisque ac mi. Morbi augue tortor, aliquam eu fringilla ac, rhoncus sed turpis. Integer in purus sem. Etiam ac tincidunt nisl.
   </p>
 </div>
 @endsection
